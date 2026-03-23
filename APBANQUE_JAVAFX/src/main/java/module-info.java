ee783e23c0e9149bb9fc19b3058dd98be6b8a6ca
BenOukil <benjioukil@gmail.com>
2026-03-23 16:06:46 +0100

    Delete APBANQUE_JAVAFX directory

--- APBANQUE_JAVAFX/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module Oukil.APBANQUE_JAVAFX {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-	requires javafx.graphics;
-
-    opens Oukil.APBANQUE_JAVAFX to javafx.fxml;
-    exports Oukil.APBANQUE_JAVAFX;
-}
